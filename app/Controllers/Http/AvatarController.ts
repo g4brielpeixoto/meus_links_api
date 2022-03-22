3e6d7dffdd8456b2f984240401333f67a8e31c48
g4briel.soarespeixoto/teksystem <g4briel.soarespeixoto@gmail.com>
2022-03-22 11:12:13 -0300

    Implementação do upload de avatar para o Dropbox

--- app/Controllers/Http/AvatarController.ts
+++ app/Controllers/Http/AvatarController.ts
@@ -3,6 +3,8 @@ import { AvatarValidator } from 'App/Validators/User'
 import Application from '@ioc:Adonis/Core/Application'
 import Database from '@ioc:Adonis/Lucid/Database'
 import fs from 'fs'
+import { Dropbox, Error, files } from 'dropbox'
+import path from 'path'
 
 export default class UserAvatarController {
   public async show({ params, response }: HttpContextContract) {
@@ -11,20 +13,42 @@ export default class UserAvatarController {
 
   public async update({ request, auth }: HttpContextContract) {
     const response = await Database.transaction(async (trx) => {
-      const { file } = await request.validate(AvatarValidator)
+      // const { file } = await request.validate(AvatarValidator)
 
       const user = auth.user!.useTransaction(trx)
 
-      if (user.avatar) fs.unlinkSync(Application.tmpPath('uploads', user.avatar))
+      // if (user.avatar) fs.unlinkSync(Application.tmpPath('uploads', user.avatar))
 
-      user.avatar = `${new Date().getTime()}.${file.extname}`
+      const dropbox = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN })
+      fs.readFile(path.join('./', 'teste.txt'), 'utf8', (err, contents) => {
+        if (err) {
+          console.log('Error: ', err)
+        }
 
-      await user.save()
-
-      await file.move(Application.tmpPath('uploads'), {
-        name: user.avatar,
-        overwrite: true
+        dropbox
+          .filesUpload({ path: '/teste.txt', contents })
+          .then((response: any) => {
+            console.log(response)
+          })
+          .catch((uploadErr: Error<files.UploadError>) => {
+            console.log(uploadErr)
+          })
       })
+
+      // user.avatar = `${new Date().getTime()}.${file.extname}`
+      // dropbox
+      //   .filesUpload({ path: `/${user.avatar}`, contents: file })
+      //   .then((response) => console.log(response))
+      //   .catch((uploadErr: Error<files.UploadError>) => {
+      //     console.log(uploadErr)
+      //   })
+
+      // await user.save()
+
+      // await file.move(Application.tmpPath('uploads'), {
+      //   name: user.avatar,
+      //   overwrite: true
+      // })
 
       return user.avatar
     })
