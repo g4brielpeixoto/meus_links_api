3352474e4a3e16dd95a070622ed587adeddce150
g4briel.soarespeixoto/teksystem <g4briel.soarespeixoto@gmail.com>
2022-03-23 11:05:55 -0300

    Novas tentativas configuração Dropbox

--- app/Controllers/Http/AvatarController.ts
+++ app/Controllers/Http/AvatarController.ts
@@ -4,7 +4,6 @@ import Application from '@ioc:Adonis/Core/Application'
 import Database from '@ioc:Adonis/Lucid/Database'
 import fs from 'fs'
 import { Dropbox, Error, files } from 'dropbox'
-import path from 'path'
 
 export default class UserAvatarController {
   public async show({ params, response }: HttpContextContract) {
@@ -12,47 +11,35 @@ export default class UserAvatarController {
   }
 
   public async update({ request, auth }: HttpContextContract) {
-    const response = await Database.transaction(async (trx) => {
-      // const { file } = await request.validate(AvatarValidator)
+    const transactionResponse = await Database.transaction(async (trx) => {
+      const { file } = await request.validate(AvatarValidator)
 
       const user = auth.user!.useTransaction(trx)
 
       // if (user.avatar) fs.unlinkSync(Application.tmpPath('uploads', user.avatar))
 
       const dropbox = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN })
-      fs.readFile(path.join('./', 'teste.txt'), 'utf8', (err, contents) => {
-        if (err) {
-          console.log('Error: ', err)
-        }
 
-        dropbox
-          .filesUpload({ path: '/teste.txt', contents })
-          .then((response: any) => {
-            console.log(response)
-          })
-          .catch((uploadErr: Error<files.UploadError>) => {
-            console.log(uploadErr)
-          })
+      user.avatar = `${new Date().getTime()}.${file.extname}`
+
+      let dropboxResponse = {} as Object
+
+      await dropbox
+        .filesUpload({ path: `/${user.avatar}`, contents: file })
+        .then((response) => (dropboxResponse = response))
+        .catch((uploadErr: Error<files.UploadError>) => (dropboxResponse = uploadErr))
+
+      await user.save()
+
+      await file.move(Application.tmpPath('uploads'), {
+        name: user.avatar,
+        overwrite: true
       })
 
-      // user.avatar = `${new Date().getTime()}.${file.extname}`
-      // dropbox
-      //   .filesUpload({ path: `/${user.avatar}`, contents: file })
-      //   .then((response) => console.log(response))
-      //   .catch((uploadErr: Error<files.UploadError>) => {
-      //     console.log(uploadErr)
-      //   })
-
-      // await user.save()
-
-      // await file.move(Application.tmpPath('uploads'), {
-      //   name: user.avatar,
-      //   overwrite: true
-      // })
-
-      return user.avatar
+      return { dropboxResponse, avatar: user.avatar }
     })
-    if (response) return { avatar: response, avatarUrl: auth.user!.avatarUrl }
+
+    return transactionResponse
   }
 
   public async destroy({ auth }: HttpContextContract) {
